d05cebd185c434c2fbc8039204e706114ef8f3ee
David <davbohr@gmail.com>
2017-02-09 13:29:27 +0100

    changes

--- custom-var.js
+++ custom-var.js
@@ -1,3 +1,3 @@
-var GOOGLE_ANALYTICS_ID = ""; // example "UA-XXXXXXXX-X"
-var CRISP_WEBSITE_ID = "";    // example "9x5x-25x-4x67-x065-x91xxxxx6x83"
-var FORMSPREE_EMAIL = "";     // example bdavidxyz@gmail.com
+var GOOGLE_ANALYTICS_ID = "UA-79244438-1"; // example "UA-XXXXXXXX-X"
+var CRISP_WEBSITE_ID = "37379e5a-25bc-4d67-b065-a91fdaef6f83";    // example "9x5x-25x-4x67-x065-x91xxxxx6x83"
+var FORMSPREE_EMAIL = "bdavidxyz@gmail.com";     // example bdavidxyz@gmail.com
